80a179dfec5054c4c8b460ad0ff71e66755f0e03
vincy1844 <vincyoneheart032@gmail.com>
2022-03-20 21:27:08 +0000

    fixes

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,6 +7,6 @@
  */
 int main()
 {
-        puts("\"with proper grammar, but the outcome is a piece of art,");
+        printf("\"with proper grammar, but the outcome is a piece of art,");
         return 0;
 }
